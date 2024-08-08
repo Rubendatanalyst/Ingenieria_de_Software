7f106bb306ad7105596574e9deb106c2195f1ef4
Rubendrl <va.rubendrl@gmail.com>
2024-08-08 14:36:28 -0400

    proy

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Proy_Sprint6
-Proyecto TT S6
+Proyecto correspondiente al sprint 6
+
+El objetivo de este proyecto es proporcionar más posibilidades de practicar las tareas habituales de la ingeniería de softwareun a través de conjunto de datos
+de anuncios de venta de coches para poder aumentar y complementar las habilidades en el campo de los datos.
+
+El proyecto incluyen la creación y gestión de entornos virtuales de Python, el desarrollo de una aplicación web y su despliegue en un servicio en la nube.
+
---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas as pd
+import plotly_express as px
+
+car_data = pd.read_csv('/Users/Rodriguez Garban/Documents/Data Analyst/Proyectos de Sprints/Proyecto_sprint_6/vehicles_us.csv')
+
+st.header('Datos de Anuncios de Ventas de Vehículos')
+hist_button = st.button('Construir histograma')
+if hist_button: # al hacer clic en el botón
+            # escribir un mensaje
+            st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+            
+            # crear un histograma
+            fig = px.histogram(car_data, x="odometer")
+        
+            # mostrar un gráfico Plotly interactivo
+            st.plotly_chart(fig, use_container_width=True)
+
+disp_button = st.button('Construir gráfica de Dispersión')
+if disp_button:
+        st.write('Creación de gráfico de Dispersión para el conjunto de datos de anuncios de venta de coches')
+        fig = px.scatter(car_data, x="odometer", y="price")
+        st.plotly_chart(fig, use_container_width=True)               
